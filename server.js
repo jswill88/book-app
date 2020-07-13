68886d52bf7026c935d5a3de74a5174b6dbed8e0
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-07-12 21:09:47 -0700

    some changes

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const dbClient = new pg.Client(process.env.DATABASE_URL);
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');

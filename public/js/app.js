62a72837124519237620db462d8ad5cfde6a3ab6
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-07-16 16:01:13 -0700

    work on sticky nav

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
 'use strict';
 
-
 $(document).ready(function () {
   $('[action^="/update"]').hide();
+  let stickyNav = $('nav').offset().top;
+  console.log(stickyNav);
+  $(window).scroll(() => {
+    console.log($(window).scrollTop);
+    if ($(window).scrollTop() > stickyNav + 20) {
+      $('nav').css('position','fixed').css('top','0');
+    } else {
+      $('nav').css('position','static');
+    }
+  });
 });
 $('#edit').click(()=> {
   $('.book-section').slideUp(1000);
